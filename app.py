95eeb6a627289df72d7a2d242790a14a8c96d83a
Yuvraj <yuvrajdake9105@gmail.com>
2026-02-12 14:33:52 +0530

    port added

--- app.py
+++ app.py
@@ -7,4 +7,6 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(debug=True ,port = port)
